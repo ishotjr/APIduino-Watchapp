773f61f1eaca41ab130ee6e0e4bf3c009a8628d9
ishotjr <github@ishotjr.com>
2016-01-10 02:28:28 -0500

    :inbox_tray: :outbox_tray: Add APIduino calls via XMLHttpRequest

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -2,6 +2,55 @@
  * pebble-js-app.js
  */
 
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function apiDuinoCall(isPut, value) {
+
+  // * * * NOT real PUT yet - both are actually GET! * * *
+
+  // construct URL
+  // TODO: add config vs. hard-coded!!!
+  var url = 'http://192.168.1.21/led';
+
+  if (isPut) {
+    url = url + '/' + value;
+  }
+
+  // send request
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+
+      // APIduino returns led value as JSON
+      // *returned value is "requested" for PUT
+      // and actual state for GET
+      var json = JSON.parse(responseText);
+
+      var led = json.led;
+      console.log('led is ' + led);
+
+      // return led value
+      var dict = {
+        'data': led
+      };
+
+      Pebble.sendAppMessage(dict, function(e) {
+        console.log('Send successful.');
+      }, function(e) {
+        console.log('Send failed!');
+      });
+
+    }      
+  );
+}
+
+
 Pebble.addEventListener('ready', function(e) {
   console.log('PebbleKit JS Ready!');
 });
@@ -9,35 +58,6 @@ Pebble.addEventListener('ready', function(e) {
 
 Pebble.addEventListener("appmessage", function(e) {
   console.log("AppMessage received!");
-  if (e.payload.verb) {
 
-    // PUT
-
-    // return requested value (NOT actual)
-    var dict = {
-      'data': e.payload.data
-    };
-
-    Pebble.sendAppMessage(dict, function(e) {
-      console.log('Send successful.');
-    }, function(e) {
-      console.log('Send failed!');
-    });
-
-  } else {
-
-    // GET
-
-    // return actual value
-    var dict = {
-      'data': 2
-    };
-
-    Pebble.sendAppMessage(dict, function(e) {
-      console.log('Send successful.');
-    }, function(e) {
-      console.log('Send failed!');
-    });
-
-  }
+  apiDuinoCall(e.payload.verb, e.payload.data);
 });
