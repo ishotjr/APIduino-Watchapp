7239daa56f1ed381ce9018fcf0a9ab3aba137488
ishotjr <github@ishotjr.com>
2016-01-10 01:05:50 -0500

    :arrows_counterclockwise: :iphone: Add AppMessage, PebbleKit JS

--- src/APIduino-Watchapp.c
+++ src/APIduino-Watchapp.c
@@ -1,7 +1,35 @@
 #include <pebble.h>
 
+#define KEY_DATA 1
+
 static Window *window;
 static TextLayer *text_layer;
+
+static char s_buffer[64];
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Message received!");
+
+  // Get the first pair
+  Tuple *data = dict_find(iterator, KEY_DATA);
+  if (data) {
+    snprintf(s_buffer, sizeof(s_buffer), "Received '%d'", data->value->uint8);
+    text_layer_set_text(text_layer, s_buffer);
+  }  
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Select");
@@ -29,6 +57,13 @@ static void window_load(Window *window) {
   text_layer_set_text(text_layer, "Press a button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  /*
+  s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
+  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_output_layer, "Waiting...");
+  text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
+  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+  */
 }
 
 static void window_unload(Window *window) {
@@ -36,6 +71,15 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage with sensible buffer sizes
+  app_message_open(1, 1);
+
   window = window_create();
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
